7bca8159cd9b8ac1dc5f3db11f52b13bd8c4c0b5
alu0100204148 <alu0100204148@ull.edu.es>
2015-02-11 16:10:17 +0000

    Actualizando Pagina

--- temperature.js
+++ temperature.js
@@ -1,23 +1,23 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calculate() {
   var result;
-  var original       = document.getElementById("........");
+  var original       = document.getElementById("original");
   var temp = original.value;
-  var regexp = /.............................../;
+  var regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([cf])\s*$/i;
   
-  var m = temp.match(......);
+  var m = temp.match(regexp);
   
   if (m) {
-    var num = ....;
-    var type = ....;
+    var num = m[1];
+    var type = m[2];
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
       result = (num * 9/5)+32;
-      result = ..............................
+      result = result + "º Farenheit";
     }
     else {
       result = (num - 32)*5/9;
-      result = ............................
+      result = result + "º Celsius";
     }
     converted.innerHTML = result;
   }
